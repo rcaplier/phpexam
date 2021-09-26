dcc41648b40a842e1c1fc8f086b03edca8764b11
Romuald Caplier <caplier.r@gmail.com>
2021-09-26 20:24:38 +0200

    Improved login form / added some constraints and CGU checkbox

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -40,7 +42,7 @@ class SecurityController extends AbstractController
     }
 
     #[Route("/register", name: "app_register")]
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher, LoginFormAuthenticator $formAuthenticator): Response
     {
         $user = new User();
         $registerForm = $this->createForm(RegisterType::class, $user);
@@ -50,7 +52,10 @@ class SecurityController extends AbstractController
             $user->setRoles(["ROLE_USER"]);
             $entityManager->persist($user);
             $entityManager->flush();
+
+            //TODO Authenticate user after registration mais si je trouve pas un truc à jour sur le net j'abandonne
         }
+
         return $this->redirectToRoute("app_login");
     }
 
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,12 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterType extends AbstractType
 {
@@ -17,16 +20,39 @@ class RegisterType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'label' => 'Email'
+                'label' => 'Email',
+                'attr' => ['class' => 'mb-3'],
+                'constraints' => new NotBlank([
+                    'message' => 'Veuillez renseigner une adresse mail valide.'
+                ])
             ])
             ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe'
+                'label' => 'Mot de passe',
+                'attr' => ['class' => 'mb-3'],
+                'constraints' => new NotBlank([
+                    'message' => 'Veuillez renseigner un mot de passe.'
+                ])
             ])
             ->add('firstName', TextType::class, [
-                'label' => 'Prénom'
+                'label' => 'Prénom',
+                'attr' => ['class' => 'mb-3'],
+                'constraints' => new NotBlank([
+                    'message' => 'Veuillez renseigner un prénom.'
+                ])
             ])
             ->add('lastName', TextType::class, [
-                'label' => 'Nom'
+                'label' => 'Nom',
+                'attr' => ['class' => 'mb-3'],
+                'constraints' => new NotBlank([
+                    'message' => 'Veuillez renseigner un nom.'
+                ])
+            ])
+            ->add('cgu',CheckboxType::class, [
+                'label' => "J'accèpte les CGU",
+                'mapped' => false,
+                'constraints' => new IsTrue([
+                    'message' => 'Veuillez accepter les CGU.'
+                ])
             ])
             ->add("save", SubmitType::class, [
                 'label' => 'Créer mon compte',
